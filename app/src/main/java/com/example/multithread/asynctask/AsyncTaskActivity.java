ee8ef9cf1fa0e6e286bdd352e62981488f6bbef6
jere <jere@motherapp.com>
2019-09-06 21:32:33 +0800

    add AsyncTask content

--- app/src/main/java/com/example/multithread/asynctask/AsyncTaskActivity.java
+++ app/src/main/java/com/example/multithread/asynctask/AsyncTaskActivity.java
@@ -1,11 +1,12 @@
 package com.example.multithread.asynctask;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.multithread.R;
@@ -13,8 +14,11 @@ import com.example.multithread.R;
 /**
  * @author jere
  */
-public class AsyncTaskActivity extends AppCompatActivity {
+public class AsyncTaskActivity extends AppCompatActivity implements View.OnClickListener {
     private ProgressBar progressBar;
+    private TextView displayTv;
+    private Button startBtn, stopBtn;
+    private MyAsyncTask myAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,51 +26,85 @@ public class AsyncTaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_async_task);
 
         progressBar = findViewById(R.id.progress_bar);
+        displayTv = findViewById(R.id.display_tv);
+        startBtn = findViewById(R.id.start_btn);
+        stopBtn = findViewById(R.id.stop_btn);
+        myAsyncTask = new MyAsyncTask();
 
-        final MyAsyncTask myAsyncTask = new MyAsyncTask();
-
-        Button startBtn = findViewById(R.id.start_progress_bar_btn);
-        startBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                myAsyncTask.execute("click start btn");
-            }
-        });
+        startBtn.setOnClickListener(this);
+        stopBtn.setOnClickListener(this);
     }
 
-    private class MyAsyncTask extends AsyncTask<String, Void, String> {
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.start_btn:
+                myAsyncTask.execute("Jere test");
+                //AsyncTask can only be executed once.
+                //so we need to block user click startBtn if AsyncTask had executed.
+                startBtn.setEnabled(false);
+                break;
+            case R.id.stop_btn:
+                myAsyncTask.cancel(true);
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 创建AsyncTask子类
+     *
+     * 继承AsyncTask类 -> AsyncTask<Params, Progress, Result>
+     * 3个泛型参数指定类型；若不使用，可用java.lang.Void类型代替
+     * Params（输入参数）:
+     * Progress（执行进度）:
+     * Result（执行结果）:
+     * 此处指定为：输入参数 = String类型、执行进度 = Integer类型、执行结果 = String类型
+     */
+    private class MyAsyncTask extends AsyncTask<String, Integer, String> {
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            for (int i = 1; i <= 5; i++){
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                progressBar.setProgress(i);
-            }
+            displayTv.setText("onPreExecute: start! ");
+            Toast.makeText(AsyncTaskActivity.this, "onPreExecute", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         protected String doInBackground(String... strings) {
-            for (int i = 1; i <= 2; i++){
+            for (int i = 1; i <= 7; i++) {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                publishProgress(i);
             }
 
-            return "jere test return from doInBackground!";
+            return " 'Result' return from doInBackground!";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressBar.setProgress(values[0]);
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Toast.makeText(AsyncTaskActivity.this, "onPostExecute", Toast.LENGTH_SHORT).show();
+            displayTv.setText("onPostExecute finished, " + s);
         }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            displayTv.setText("onCancelled");
+            progressBar.setProgress(0);
+        }
+
     }
 
 }
