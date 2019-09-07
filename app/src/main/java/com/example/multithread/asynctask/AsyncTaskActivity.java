a64f7f11fb8786c59ca2709f4348a3bc07c81f5f
jere <jere@motherapp.com>
2019-09-07 14:49:13 +0800

    completed asynctask part

--- app/src/main/java/com/example/multithread/asynctask/AsyncTaskActivity.java
+++ app/src/main/java/com/example/multithread/asynctask/AsyncTaskActivity.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 
 import com.example.multithread.R;
 
+import java.util.Arrays;
+
 /**
  * @author jere
  */
@@ -40,13 +42,13 @@ public class AsyncTaskActivity extends AppCompatActivity implements View.OnClick
         switch (view.getId()) {
             case R.id.start_btn:
                 myAsyncTask.execute("Jere test");
-                //AsyncTask can only be executed once.
-                //so we need to block user click startBtn if AsyncTask had executed.
+                //AsyncTask只能执行一次，如果你在AsyncTask执行过程中再次调用 execute() 执行它，就会跳IllegalStateException异常
+                //java.lang.IllegalStateException: Cannot execute task: the task is already running.
                 startBtn.setEnabled(false);
                 break;
             case R.id.stop_btn:
+                //中断线程执行
                 myAsyncTask.cancel(true);
-
                 break;
             default:
                 break;
@@ -54,17 +56,17 @@ public class AsyncTaskActivity extends AppCompatActivity implements View.OnClick
     }
 
     /**
-     * 创建AsyncTask子类
-     *
-     * 继承AsyncTask类 -> AsyncTask<Params, Progress, Result>
-     * 3个泛型参数指定类型；若不使用，可用java.lang.Void类型代替
-     * Params（输入参数）:
-     * Progress（执行进度）:
-     * Result（执行结果）:
-     * 此处指定为：输入参数 = String类型、执行进度 = Integer类型、执行结果 = String类型
+     * 创建MyAsyncTask类, 继承AsyncTask类 -> AsyncTask<Params, Progress, Result>
+     * 3个泛型参数指定类型:
+     * Params（输入参数）: String类型
+     * Progress（执行进度）: Integer类型
+     * Result（执行结果）: String类型
      */
     private class MyAsyncTask extends AsyncTask<String, Integer, String> {
 
+        /**
+         * 在后台任务开始计算执行前执行onPreExecute()操作
+         */
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -72,37 +74,62 @@ public class AsyncTaskActivity extends AppCompatActivity implements View.OnClick
             Toast.makeText(AsyncTaskActivity.this, "onPreExecute", Toast.LENGTH_SHORT).show();
         }
 
+        /**
+         * 接收输入参数，执行后台任务中的计算工作（耗时操作），计算结束，返回计算结果
+         * @param strings Params输入参数，在主线程执行execute()传入的参数
+         * @return 返回执行结果给onPostExecute()
+         */
         @Override
         protected String doInBackground(String... strings) {
-            for (int i = 1; i <= 7; i++) {
+            for (int i = 1; i <= 10; i++) {
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                //通过调用publishProgress()方法，将执行进度传递给onProgressUpdate()
                 publishProgress(i);
             }
 
-            return " 'Result' return from doInBackground!";
+            String result;
+            result = Arrays.toString(strings) + " return from doInBackground";
+            return result;
         }
 
+        /**
+         * 接收后台计算执行进度，在UI线程中通过ProgressBar现实执行进度。
+         * @param values ： 执行进度，通过publishProgress()传入
+         */
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
+            //得到一个整型数组，但每次里面只有一个元素，所有通过values[0]就可以拿到当前的执行进度
             progressBar.setProgress(values[0]);
+            displayTv.setText("onProgressUpdate: value = " + values[0]);
         }
 
+        /**
+         * 接收后台任务计算结束返回的结果，在UI线程上显示出来
+         * @param s ：后台计算结束返回的执行结果，由doInBackground()返回
+         */
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            displayTv.setText("onPostExecute finished, " + s);
+            displayTv.setText("onPostExecute: " + s);
+            Toast.makeText(AsyncTaskActivity.this, "onPostExecute", Toast.LENGTH_SHORT).show();
         }
 
+        /**
+         * 在主线程中调用cancel()方法，执行onCancelled()，取消执行后台任务
+         *
+         * 当任务计算完成时，无法取消任务，或者已经取消任务后不可再次取消
+         */
         @Override
         protected void onCancelled() {
             super.onCancelled();
             displayTv.setText("onCancelled");
             progressBar.setProgress(0);
+            Toast.makeText(AsyncTaskActivity.this, "onCancelled", Toast.LENGTH_SHORT).show();
         }
 
     }
